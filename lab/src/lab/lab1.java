e7f008467f48f3d2b50495c0989e344141c59e13
sakinabagalkot <bakinasagalkot@gmail.com>
2025-09-02 23:00:23 +0400

    Feature B added

--- lab/src/lab/lab1.java
+++ lab/src/lab/lab1.java
@@ -3,5 +3,11 @@ package lab;
 public class lab1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        
+        featureB();
+    }
+    
+    public static void featureB() {
+    	System.out.println("Im adding feature_B");
     }
 }
