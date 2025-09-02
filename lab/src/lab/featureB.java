568e9e17d7a9f83b7624ea41849dc38d88d3325c
shathahsaleem <shathahsaleem@gmail.com>
2025-09-02 23:06:39 +0400

    Feature B added

---
+++ lab/src/lab/featureB.java
@@ -0,0 +1,10 @@
+package lab;
+
+public class featureB {
+
+	    public static void main(String[] args) {
+	        System.out.println("Hi from Shahd, Sakina, and Shathah!");
+	
+
+}
+}
--- lab/src/lab/lab1.java
+++ lab/src/lab/lab1.java
@@ -3,5 +3,15 @@ package lab;
 public class lab1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        
+        greet("Sahar", 1234);
     }
+    
+    
+    public static void greet(String name, int ID) {
+        System.out.println("Hello, " + name + " with ID " + ID + "!");
+    }
+    
 }
+
+
